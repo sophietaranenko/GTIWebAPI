2dd6e921bda0a68cad5939eeebd6c3680958e379
sophietaranenko <sophytaranenko@gmail.com>
2016-12-05 11:00:34 +0200

    Token days = 1 day instead of 2 weeks

--- GTIWebAPI/App_Start/Startup.Auth.cs
+++ GTIWebAPI/App_Start/Startup.Auth.cs
@@ -49,7 +49,7 @@ namespace GTIWebAPI
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
