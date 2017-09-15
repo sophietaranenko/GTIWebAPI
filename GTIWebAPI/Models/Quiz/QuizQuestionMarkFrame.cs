693e766fc6fb58f66774144510ab941ff7e14118
Pavel Plakhotniuk <pavel.plachotnjuk@gmail.com>
2017-09-15 12:25:11 +0300

    fucking revert

---
+++ GTIWebAPI/Models/Quiz/QuizQuestionMarkFrame.cs
@@ -0,0 +1,46 @@
+﻿namespace GTIWebAPI.Models.Quiz
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("QuizQuestionMarkFrame")]
+    public class QuizQuestionMarkFrame
+    {
+        public int Id { get; set; }
+
+        public int? QuizQuestionId { get; set; }
+
+        public int? LowerValue { get; set; }
+
+        public int? UpperValue { get; set; }
+
+        public virtual QuizQuestion Question { get; set; }
+
+        public QuizQuestionMarkFrameDTO ToDTO()
+        {
+            QuizQuestionMarkFrameDTO dto = new QuizQuestionMarkFrameDTO()
+            {
+                Id = this.Id,
+                QuizQuestionId = this.QuizQuestionId,
+                LowerValue = this.LowerValue,
+                UpperValue = this.UpperValue
+            };
+            return dto;
+        }
+    }
+
+    public class QuizQuestionMarkFrameDTO
+    {
+        public int Id { get; set; }
+
+        public int? QuizQuestionId { get; set; }
+
+        public int? LowerValue { get; set; }
+
+        public int? UpperValue { get; set; }
+    }
+}
